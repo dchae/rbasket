fae2cfa486552377eb7fb4e9cfb5e767bee2ac57
Daniel Chae <daniel.jiwoong@gmail.com>
2025-05-22 06:58:36 +0900

    feat: added getAWSSecret util method

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -2,6 +2,10 @@ import { IncomingHttpHeaders } from "http";
 import { QueryResult } from "pg";
 import PostgresController from "./controllers/postgresql";
 import { Request, PostgresRequestRow } from "./types";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 
 export function generateRandomString() {
   return Math.random().toString(36).substring(2);
@@ -16,7 +20,7 @@ export async function generateToken(): Promise<string> {
   try {
     do {
       const segments: string[] = Array.from({ length: 3 }, () =>
-        generateRandomString()
+        generateRandomString(),
       );
       token = segments.join("");
 
@@ -46,4 +50,30 @@ export function normalizeRequest(request: PostgresRequestRow): Request {
     headers: request["headers"],
     bodyMongoId: request["body_mongo_id"],
   };
+}
+
+// const secret_name = "rds-psql";
+// region: "ap-northeast-2",
+export async function getAWSSecret(secret_name: string, region: string) {
+  const client = new SecretsManagerClient({ region });
+
+  let response;
+
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      }),
+    );
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+  }
+
+  const secretStr = response.SecretString;
+  console.log(secretStr);
+
+  return JSON.parse(secretStr ?? "{}");
 }
